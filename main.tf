526c4d16a707645f9d168d7ebfed281bd7e03429
SHIBASHIS <shibashis.dutta@outlook.com>
2023-06-08 14:52:04 +0530

    CUSTOMER BILLING

--- main.tf
+++ main.tf
@@ -39,11 +39,21 @@ resource "snowflake_schema" "schema" {
   name                = "CVS_DB_SCHEMA"
 }
 
+resource "snowflake_schema" "customer_schema" {
+  database            = "CVS_PROJ_BILLING_DATA"
+  name                = "CVS_DB_SCHEMA_CUST"
+}
+
 resource "snowflake_sequence" "sequence" {
   database = snowflake_schema.schema.database
   schema   = snowflake_schema.schema.name
   name     = "CVS_DB_SEQUENCE"
 }
+resource "snowflake_sequence" "customer_sequence" {
+  database = snowflake_schema.schema.database
+  schema   = snowflake_schema.schema.name
+  name     = "CVS_DB_SEQUENCE_CUST"
+} 
 
 resource "snowflake_table" "table" {
   database            = snowflake_schema.schema.database
@@ -67,16 +77,75 @@ resource "snowflake_table" "table" {
     nullable = false
   }
 
-  column {
-    name = "employee_DOJ"
-    type = "TIMESTAMP_NTZ(9)"
-  }
 }
 
 resource "snowflake_table_grant" "grant" {
   database_name = "CVS_EMPLOYEE_DATA"
   schema_name   = "CVS_DB_SCHEMA"
   table_name    = "EMPLOYEE"
+  privilege = "ALL PRIVILEGES"
+  roles     = ["ACCOUNTADMIN"]
+}
+
+
+resource "snowflake_table" "customer" {
+  database            = snowflake_schema.customer_schema.database
+  schema              = snowflake_schema.customer_schema.name
+  name                = "CUSTOMER_BILLING"
+  column {
+    name     = "CustID"
+    type     = "varchar"
+    nullable = false
+  }
+
+  column {
+    name     = "Company"
+    type     = "varchar"
+    nullable = false
+  }
+
+  column {
+    name     = "Address"
+    type     = "varchar"
+    nullable = false
+  }
+
+   column {
+    name     = "City"
+    type     = "varchar"
+    nullable = false
+  }
+
+   column {
+    name     = "State"
+    type     = "varchar"
+    nullable = false
+  }
+
+   column {
+    name     = "Zip"
+    type     = "varchar"
+    nullable = false
+  }
+
+   column {
+    name     = "Country"
+    type     = "varchar"
+    nullable = false
+  }
+
+  column {
+    name     = "Phone"
+    type     = "varchar"
+    nullable = false
+  }
+
+}
+
+resource "snowflake_table_grant" "grant_cust" {
+  database_name = "CVS_PROJ_BILLING_DATA"
+  schema_name   = "CVS_DB_SCHEMA_CUST"
+  table_name    = "CUSTOMER_BILLING"
   privilege = "ALL PRIVILEGES"
   roles     = ["ACCOUNTADMIN"]
 }
