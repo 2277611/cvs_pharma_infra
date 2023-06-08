5efe696227ed37556753b13fd0490da5c7db6277
SHIBASHIS <shibashis.dutta@outlook.com>
2023-06-08 12:25:29 +0530

    req

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
-    
+
     snowflake = {
-      source = "Snowflake-Labs/snowflake"
+      source  = "Snowflake-Labs/snowflake"
       version = "~> 0.65.0"
     }
   }
@@ -17,34 +17,34 @@ terraform {
 }
 
 resource "snowflake_warehouse" "warehouse" {
-  name = "CVS_WH"
+  name           = "CVS_WH"
   warehouse_size = "LARGE"
-  auto_suspend = 600
+  auto_suspend   = 600
 }
 
 resource "snowflake_database" "db" {
-  name = "CVS_DB"
-  comment = "test CVS_DB"
+  name                        = "CVS_DB"
+  comment                     = "test CVS_DB"
   data_retention_time_in_days = 1
 }
 
 resource "snowflake_schema" "schema" {
   database            = "snowflake_database.db.name"
-  name                = "CVS_DB_TABLE_schema"
+  name                = "CVS_DB_SCHEMA"
   data_retention_days = 1
 }
 
 resource "snowflake_sequence" "sequence" {
   database = snowflake_schema.schema.database
   schema   = snowflake_schema.schema.name
-  name     = "CVS_DB_TABLE_sequence"
+  name     = "CVS_DB_SEQUENCE"
 }
 
 resource "snowflake_table" "table" {
   database            = snowflake_schema.schema.database
   schema              = snowflake_schema.schema.name
-  name                = "CVS_DB_TABLE"
-  comment             = "CVS_DB_TABLE"
+  name                = "EMPLOYEE"
+  comment             = "Employee Table"
   cluster_by          = ["employee_dept"]
   data_retention_days = snowflake_schema.schema.data_retention_days
   change_tracking     = false
@@ -71,16 +71,16 @@ resource "snowflake_table" "table" {
     name = "employee_ DOJ"
     type = "TIMESTAMP_NTZ(9)"
   }
-} 
+}
 
 resource "snowflake_database" "db_post" {
-  name = "CVS_DB_POST"
-  comment = "test CVS_DB"
+  name                        = "CVS_DB_POST"
+  comment                     = "test CVS_DB"
   data_retention_time_in_days = 3
 }
 
 resource "snowflake_database" "db_test" {
-  name = "CVS_DB_TEST"
-  comment = "test CVS_DB"
+  name                        = "CVS_DB_TEST"
+  comment                     = "test CVS_DB"
   data_retention_time_in_days = 3
 }
