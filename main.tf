8e313a926169181ded586f5dd1d52c7b93c4a2a5
2277611_shibashis_dutta <135108112+2277611@users.noreply.github.com>
2023-06-09 20:46:52 +0530

    removed customer

--- main.tf
+++ main.tf
@@ -31,69 +31,33 @@ resource "snowflake_database" "employee_db" {
 }
 
 resource "snowflake_database_grant" "employee_db_grant" {
-  database_name = "CVS_EMPLOYEE_DATA"
+  database_name = snowflake_database.employee_db.name
   privilege = "ALL PRIVILEGES"
   roles     = ["ACCOUNTADMIN","ORGADMIN"]
 }
 
 resource "snowflake_schema" "employee_schema" {
-  database            = "CVS_EMPLOYEE_DATA"
+  database            = snowflake_database.employee_db.name
   name                = "CVS_EMPLOYEE_SCHEMA"
 }
 
 resource "snowflake_schema_grant" "employee_schema_grant" {
-  database_name = "CVS_EMPLOYEE_DATA"
+  database_name = snowflake_database.employee_db.name
   schema_name   = snowflake_schema.employee_schema.name
   privilege = "ALL PRIVILEGES"
   roles     = ["ACCOUNTADMIN","ORGADMIN"]
 }
 
 resource "snowflake_sequence" "employee_sequence" {
-  database =  "CVS_EMPLOYEE_DATA"
+  database =  "snowflake_database.employee_db.name
   schema   = snowflake_schema.employee_schema.name
   name     = "CVS_EMPLOYEE_SEQUENCE"
 }
 
 resource "snowflake_sequence_grant" "employee_sequence_grant" {
-  database_name = "CVS_EMPLOYEE_DATA"
+  database_name = snowflake_database.employee_db.name
   schema_name   = snowflake_schema.employee_schema.name
   sequence_name = snowflake_sequence.employee_sequence.name
-  privilege = "ALL PRIVILEGES"
-  roles     = ["ACCOUNTADMIN","ORGADMIN"]
-}
-
-resource "snowflake_database" "billing_db" {
-  name = "CVS_PROJ_BILLING_DATA"
-}
-
-resource "snowflake_database_grant" "billing_db_grant" {
-  database_name = snowflake_database.billing_db.name
-  privilege = "ALL PRIVILEGES"
-  roles     = ["ACCOUNTADMIN","ORGADMIN"]
-}
-
-resource "snowflake_schema" "customer_schema" {
-  database            = snowflake_database.billing_db.name
-  name                = "CVS_CUSTOMER_SCHEMA"
-}
-
-resource "snowflake_schema_grant" "customer_schema_grant" {
-  database_name = snowflake_database.billing_db.name
-  schema_name   = snowflake_schema.customer_schema.name
-  privilege = "ALL PRIVILEGES"
-  roles     = ["ACCOUNTADMIN","ORGADMIN"]
-}
-
-resource "snowflake_sequence" "customer_sequence" {
-  database = snowflake_database.billing_db.name
-  schema   = snowflake_schema.customer_schema.name
-  name     = "CVS_CUSTOMER_SEQUENCE"
-}
-
-resource "snowflake_sequence_grant" "customer_sequence_grant" {
-  database_name = snowflake_database.billing_db.name
-  schema_name   = snowflake_schema.customer_schema.name
-  sequence_name = snowflake_sequence.customer_sequence.name
   privilege = "ALL PRIVILEGES"
   roles     = ["ACCOUNTADMIN","ORGADMIN"]
 }
@@ -126,69 +90,6 @@ resource "snowflake_table_grant" "employee_table_grant" {
   database_name = "CVS_EMPLOYEE_DATA"
   schema_name   = "CVS_EMPLOYEE_SCHEMA"
   table_name    = "EMPLOYEE"
-  privilege = "ALL PRIVILEGES"
-  roles     = ["ACCOUNTADMIN"]
-}
-
-
-resource "snowflake_table" "customer_table" {
-  database            = snowflake_database.billing_db.name
-  schema              = snowflake_schema.customer_schema.name
-  name                = "CUSTOMER_BILLING"
-  column {
-    name     = "CustID"
-    type     = "varchar"
-    nullable = false
-  }
-
-  column {
-    name     = "Company"
-    type     = "varchar"
-    nullable = false
-  }
-
-  column {
-    name     = "Address"
-    type     = "varchar"
-    nullable = false
-  }
-
-   column {
-    name     = "City"
-    type     = "varchar"
-    nullable = false
-  }
-
-   column {
-    name     = "State"
-    type     = "varchar"
-    nullable = false
-  }
-
-   column {
-    name     = "Zip"
-    type     = "varchar"
-    nullable = false
-  }
-
-   column {
-    name     = "Country"
-    type     = "varchar"
-    nullable = false
-  }
-
-  column {
-    name     = "Phone"
-    type     = "varchar"
-    nullable = false
-  }
-
-}
-
-resource "snowflake_table_grant" "grant_cust" {
-  database_name = "CVS_PROJ_BILLING_DATA"
-  schema_name   = "CVS_CUSTOMER_SCHEMA"
-  table_name    = "CUSTOMER_BILLING"
   privilege = "ALL PRIVILEGES"
   roles     = ["ACCOUNTADMIN"]
 }
