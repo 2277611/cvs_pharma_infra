01d6f8809b09a6bf317f6b022597d214c0a11494
2277611_shibashis_dutta <135108112+2277611@users.noreply.github.com>
2023-06-09 19:52:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,15 +19,30 @@ resource "snowflake_database" "employee_db" {
   name = "CVS_EMPLOYEE_DATA"
 }
 
+resource "snowflake_database" "billing_db" {
+  name = "CVS_PROJ_BILLING_DATA"
+}
+
 resource "snowflake_schema" "employee_schema" {
   database = "CVS_EMPLOYEE_DATA"
   name = "CVS_EMPLOYEE_SCHEMA"
+}
+
+resource "snowflake_schema" "customer_schema" {
+  database = CVS_PROJ_BILLING_DATA
+  name = "CVS_CUSTOMER_SCHEMA"
 }
 
 resource "snowflake_sequence" "employee_sequence" {
   database = "CVS_EMPLOYEE_DATA"
   schema = "CVS_EMPLOYEE_SCHEMA"
   name = "CVS_EMPLOYEE_SEQUENCE"
+}
+
+resource "snowflake_sequence" "customer_sequence" {
+  database = "CVS_PROJ_BILLING_DATA"
+  schema   = "CVS_CUSTOMER_SCHEMA"
+  name     = "CVS_CUSTOMER_SEQUENCE"
 }
 
 
